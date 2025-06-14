9262683c230a3a824d681332143110ae3447189f
gowtham-009 <139350157+gowtham-009@users.noreply.github.com>
2025-06-14 13:46:30 +0530

    dd

--- pages/ipvlogin.vue
+++ pages/ipvlogin.vue
@@ -8,7 +8,7 @@
 
 <script setup>
 import { onMounted } from 'vue';
-
+const { baseurl } = globalurl();
 onMounted(() => {
   const queryString = window.location.search; // e.g. "?NDUw"
   console.log('vj', queryString);
@@ -39,6 +39,7 @@ onMounted(() => {
     if (!isNaN(decodedNumber)) {
       console.log('✅ Base64 Value:', value);
       console.log('🔢 Decoded Number:', decodedNumber);
+      routeComponents(value)
     } else {
       console.log('✅ Base64 Value:', value);
       console.log('🔤 Decoded String:', decoded);
@@ -47,5 +48,38 @@ onMounted(() => {
     console.log('❌ Not a Base64 value:', value);
   }
 });
+
+
+
+const routeComponents = async (token) => {
+  const user = encryptionrequestdata({
+    userToken: token
+  });
+
+  const payload = { payload: user };
+  const jsonString = JSON.stringify(payload);
+  const apiurl = `${baseurl.value}ipv_login`;
+
+  try {
+    const response = await fetch(apiurl, {
+      method: 'POST',
+      headers: {
+        'Authorization': 'C58EC6E7053B95AEF7428D9C7A5DB2D892EBE2D746F81C0452F66C8920CDB3B1',
+        'Content-Type': 'application/json',
+      },
+      body: jsonString,
+    });
+
+    if (!response.ok) {
+      throw new Error(`Network request failed with status ${response.status}`);
+    }
+
+    const data = await response.json();
+      
+  } catch (error) {
+    console.error("Error saving nominee:", error.message);
+ 
+  }
+};
 
 </script>
